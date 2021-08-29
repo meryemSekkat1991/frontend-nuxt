d0dc339257aa8b008b9fd3d99cb3d7993362dfa4
Meryem Sekkat <meryem@pre-history.com>
2021-08-29 20:29:08 +0200

    packages

---
+++ dist/_nuxt/static/1630260856/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"frontend-nuxt\u002F_nuxt\u002Fstatic\u002F1630260856",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"frontend-nuxt\u002F",assetsPath:"frontend-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
